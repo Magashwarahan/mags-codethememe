f014caf4124f2466e680428445c7f538fa96232e
Magashwarahan <magasharul25@gmail.com>
2021-07-10 22:02:19 +0530

    icon removed

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000
+const port = 3000;
 const fetch = require('node-fetch');
 
 app.use(express.static('public'));
 
 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
   
 app.get('/dinoname', async (request, response) => {
 	const fetchApi = await fetch(
